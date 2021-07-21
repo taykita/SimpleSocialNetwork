8964167ded23afc47e96cae26164181883a863ab
taykita <nordac2001@gmail.com>
2021-07-21 14:53:50 +0300

    fix

--- src/main/java/source/entity/FriendListServlet.java
+++ src/main/java/source/entity/FriendListServlet.java
@@ -30,7 +30,6 @@ public class FriendListServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = resp.getWriter();
 
         Context context = new Context(new Locale("ru"));
         List<Account> allUsers = dataBase.getAll();
