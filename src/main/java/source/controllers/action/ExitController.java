957b1bcd7fa6c630052128c400bd66114085229e
taykita <nordac2001@gmail.com>
2021-08-05 00:10:55 +0300

    Добавил кнопку выхода

---
+++ src/main/java/source/controllers/action/ExitController.java
@@ -0,0 +1,16 @@
+package source.controllers.action;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class ExitController {
+    @GetMapping("/exit")
+    public String exit(HttpSession session) {
+        session.invalidate();
+        return "redirect: login";
+    }
+}
