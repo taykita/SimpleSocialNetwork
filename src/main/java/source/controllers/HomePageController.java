6f7d8a7a705555907b8bab5c25bdc620d674c1c3
taykita <nordac2001@gmail.com>
2021-08-16 13:02:07 +0300

    home page

---
+++ src/main/java/source/controllers/HomePageController.java
@@ -0,0 +1,25 @@
+package source.controllers;
+
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import source.controllers.entity.Account;
+
+@Controller
+public class HomePageController {
+
+@GetMapping("/home")
+public String homePage(Model model){
+
+    if (SecurityContextHolder.getContext().getAuthentication().getPrincipal() instanceof Account) {
+        model.addAttribute("isAuth", true);
+    } else {
+        model.addAttribute("isAuth", false);
+    }
+
+    return "home";
+}
+
+
+}
