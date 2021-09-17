f27e600e306d3709fbdf755195eb27f9634a1412
taykita <artem.korovkin34@gmail.com>
2021-09-17 16:23:28 +0300

    Small fix

--- src/main/java/source/controllers/authorization/AuthorizationController.java
+++ src/main/java/source/controllers/authorization/AuthorizationController.java
@@ -11,14 +11,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import source.controllers.entity.Account;
 import source.controllers.entity.User;
-import source.database.AccountRepository;
-import source.database.ChatRepository;
 import source.exception.AccStorageException;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @Controller
 public class AuthorizationController {
@@ -32,23 +27,8 @@ public class AuthorizationController {
     @PostMapping("/sign")
     public String registerUser(@ModelAttribute("account") @Valid Account account,
                                BindingResult bindingResult, @RequestParam String chPass, Model model) throws AccStorageException {
-        if (bindingResult.hasErrors())
-            return "sign-in";
 
-        if (account.getPass().equals(chPass)) {
-            return registration(account);
-        } else {
-            return "redirect:" + "sign";
-        }
-    }
-
-    private String registration(Account account) throws AccStorageException {
-        if (authorizationService.existAccount(account.getEmail())) {
-            return "redirect:" + "sign";
-        } else {
-            authorizationService.addAccount(account);
-            return "redirect:" + "login";
-        }
+        return authorizationService.registration(account, bindingResult, chPass);
     }
 
     @GetMapping("/sign")
@@ -61,20 +41,19 @@ public class AuthorizationController {
     public String showLoginPage(Model model,
                                 @RequestParam(value = "error", required = false) String error,
                                 @RequestParam(value = "logout", required = false) String logout) {
-        if (SecurityContextHolder.getContext().getAuthentication().getPrincipal() instanceof User) {
+
+        if (checkAuth()) {
             return "redirect:" + "main";
         }
 
-        if (error != null) {
-            model.addAttribute("error", "Неправильное имя пользователя или пароль!");
-        }
-
-        if (logout != null) {
-            model.addAttribute("logout", "Вы вышли!");
-        }
+        authorizationService.checkErrorAndLogout(model, error, logout);
 
         model.addAttribute("account", new Account());
         return "log-in";
+    }
+
+    private boolean checkAuth() {
+        return SecurityContextHolder.getContext().getAuthentication().getPrincipal() instanceof User;
     }
 
 }
--- src/main/java/source/controllers/authorization/AuthorizationService.java
+++ src/main/java/source/controllers/authorization/AuthorizationService.java
@@ -2,6 +2,8 @@ package source.controllers.authorization;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import source.controllers.entity.Account;
 import source.controllers.entity.chat.ChatType;
 import source.database.AccountRepository;
@@ -22,14 +24,45 @@ public class AuthorizationService {
     private final ChatRepository chatRepository;
     private final AccountRepository accountRepository;
 
-    public void addAccount(Account account) throws AccStorageException {
+    public String registration(Account account, BindingResult bindingResult, String chPass) throws AccStorageException {
+        if (bindingResult.hasErrors())
+            return "sign-in";
+
+        if (account.getPass().equals(chPass)) {
+            return checkExistAndCreateAcc(account);
+        } else {
+            return "redirect:" + "sign";
+        }
+    }
+
+    public void checkErrorAndLogout(Model model, String error, String logout) {
+        if (error != null) {
+            model.addAttribute("error", "Неправильное имя пользователя или пароль!");
+        }
+
+        if (logout != null) {
+            model.addAttribute("logout", "Вы вышли!");
+        }
+    }
+
+    private String checkExistAndCreateAcc(Account account) throws AccStorageException {
+        if (existAccount(account.getEmail())) {
+            return "redirect:" + "sign";
+        } else {
+            addAccount(account);
+            return "redirect:" + "login";
+        }
+    }
+
+    private void addAccount(Account account) throws AccStorageException {
         account = accountRepository.addAccount(account);
         List<Integer> ids = new ArrayList<>();
         ids.add(account.getId());
         chatRepository.addChat(ids, "Сохраненные сообщения", ChatType.SAVED);
     }
 
-    public boolean existAccount(String email) throws AccStorageException {
+    private boolean existAccount(String email) throws AccStorageException {
         return accountRepository.existAccount(email);
     }
+
 }
