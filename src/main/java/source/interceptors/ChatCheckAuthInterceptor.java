23eecf633e40fe0b88907d01c279e6a8546d9a76
taykita <nordac2001@gmail.com>
2021-10-19 16:40:52 +0300

    Fix

--- src/main/java/source/interceptors/ChatCheckAuthInterceptor.java
+++ src/main/java/source/interceptors/ChatCheckAuthInterceptor.java
@@ -22,13 +22,17 @@ public class ChatCheckAuthInterceptor extends HandlerInterceptorAdapter {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         int chatId = Integer.parseInt(request.getParameter("chatId"));
-        UsernamePasswordAuthenticationToken authenticationToken = (UsernamePasswordAuthenticationToken) request.getUserPrincipal();
-        User activeUser = (User) authenticationToken.getPrincipal();
+        User activeUser = getActiveUser(request);
         if (authUser(chatId, activeUser.getId())) {
             return true;
         }
         response.sendRedirect("chat-list");
         return false;
+    }
+
+    private User getActiveUser(HttpServletRequest request) {
+        UsernamePasswordAuthenticationToken authenticationToken = (UsernamePasswordAuthenticationToken) request.getUserPrincipal();
+        return (User) authenticationToken.getPrincipal();
     }
 
     public boolean authUser(int chatId, int accId) throws AccStorageException {
