cb4486cfdf5c89d83a9c6b63f5db3d588836f68f
taykita <nordac2001@gmai.com>
2022-09-12 21:23:24 +0300

    Продолжение разработки. Починил работу со статическими ресурсами

--- src/main/java/source/configuration/SpringConfig.java
+++ src/main/java/source/configuration/SpringConfig.java
@@ -50,12 +50,16 @@ public class SpringConfig implements WebMvcConfigurer {
     @Value("${s3.signingRegion}")
     private String signingRegion;
 
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/"
+    };
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/resources/**")
-                .addResourceLocations("/resources/");
+        registry.addResourceHandler("/**")
+                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
     }
-
     @Bean
     public SessionFactory sessionFactory() {
         return getSessionFactory();
